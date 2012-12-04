358e30e4bc40df928a257bdb16dcb2100fa2d301
Yevgeniy Valeyev <yevgeniyvaleyev@ubuntu.(none)>
2012-12-04 11:54:56 +0200

    angular webworker servise example

---
+++ angularjs-webworker-service.js
@@ -0,0 +1,20 @@
+angular.module('istackApp.services', ['ngResource'])
+
+.factory('atomFieldWorker', ['$q', '$rootScope', function($q, $rootScope) {
+
+    var worker = new Worker("/scripts/resolver.worker.js");
+
+    return {
+        resolve: function(_atom) {
+            var _defer = $q.defer();
+            worker.addEventListener("message", function(event) {
+                $rootScope.$apply(function() {
+                    console.log(event.data)
+                    _defer.resolve(event.data);
+                });
+            }, false);
+            worker.postMessage(_atom);
+            return _defer.promise;
+        }
+    }
+}])
