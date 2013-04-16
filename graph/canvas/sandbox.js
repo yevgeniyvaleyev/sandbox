d3c3bad93803ad8dba0c3105c687f4c8700d8a9b
Yevgeniy Valeyev <yvaleyev@isightpartners.com>
2013-04-16 16:24:24 +0200

    added examples about colors, alpha, lines, images, etc

--- graph/canvas/sandbox.js
+++ graph/canvas/sandbox.js
@@ -15,6 +15,76 @@ function addCanvas(id) {
   return canvas;
 }
 
+function Lines() {
+  var ctx = addCanvas().getContext("2d");
+  for (var i = 0; i < 10; i++){
+    ctx.lineWidth = 1+i;
+    ctx.beginPath();
+    ctx.moveTo(5+i*14,5);
+    ctx.lineTo(5+i*14,140);
+    ctx.stroke();
+  }
+}
+to_load.push(Lines)
+
+function Alpha() {
+  var ctx = addCanvas().getContext("2d");
+  ctx.fillStyle = '#FD0';
+  ctx.fillRect(0,0,75,75);
+  ctx.fillStyle = '#6C0';
+  ctx.fillRect(75,0,75,75);
+  ctx.fillStyle = '#09F';
+  ctx.fillRect(0,75,75,75);
+  ctx.fillStyle = '#F30';
+  ctx.fillRect(75,75,75,75);
+  ctx.fillStyle = '#FFF';
+ 
+  // set transparency value
+  ctx.globalAlpha = 0.2;
+ 
+  // Draw semi transparent circles
+  for (i = 0; i < 7; i++){
+    ctx.beginPath();
+    ctx.arc(75, 75, 10 + 10 * i, 0, Math.PI * 2, true);
+    ctx.fill();
+  }
+
+  ctx.globalAlpha = 1.0;
+  // Draw background
+  ctx.fillStyle = 'rgb(255,221,0)';
+  ctx.fillRect(151,0,300,37.5);
+  ctx.fillStyle = 'rgb(102,204,0)';
+  ctx.fillRect(151,37.5,300,37.5);
+  ctx.fillStyle = 'rgb(0,153,255)';
+  ctx.fillRect(151,75,300,37.5);
+  ctx.fillStyle = 'rgb(255,51,0)';
+  ctx.fillRect(151,112.5,300,37.5);
+ 
+  // Draw semi transparent rectangles
+  for (var i=0;i<10;i++){
+    ctx.fillStyle = 'rgba(255,255,255,'+(i+1)/10+')';
+    for (var j=0;j<4;j++){
+      ctx.fillRect(156+i*14, 5+j*37.5, 14, 27.5)
+    }
+  }
+}
+to_load.push(Alpha)
+
+function ColorsCircles() {
+  var ctx = addCanvas().getContext("2d");
+  for (var i = 0; i < 6; i++) {
+    for (var j = 0; j < 6; j++) {
+      ctx.strokeStyle = 'rgb(0,' + Math.floor(255 - 42.5 * i) + ',' + Math.floor(255 - 42.5 * j) + ')';
+      ctx.fillStyle = 'rgba(' + Math.floor(255 - 42.5 * i) + ',' + Math.floor(255 - 42.5 * j) + ',0, 0.4)';
+      ctx.beginPath();
+      ctx.arc(12.5 + j * 25, 12.5 + i * 25, 10, 0, Math.PI * 2, true);
+      ctx.fill();
+      ctx.stroke();
+    }
+  }
+}
+to_load.push(ColorsCircles)
+
 function Squares() {
   var canvas = addCanvas();
   if (canvas.getContext) {
@@ -25,8 +95,8 @@ function Squares() {
     ctx.fillRect(30, 30, 55, 50);
 
     ctx.fillRect(25 + 150, 25, 100, 100);
-    ctx.clearRect(45+ 150, 45, 60, 60);
-    ctx.strokeRect(50+ 150, 50, 50, 50);
+    ctx.clearRect(45 + 150, 45, 60, 60);
+    ctx.strokeRect(50 + 150, 50, 50, 50);
   }
 }
 to_load.push(Squares)
@@ -53,19 +123,19 @@ to_load.push(Triangles)
 
 function Circles() {
   var ctx = addCanvas().getContext('2d');
-  for(var i=0;i<3;i++){
-    for(var j=0;j<4;j++){
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 4; j++) {
       ctx.beginPath();
-      var x              = 25+j*50;               // x coordinate
-      var y              = 25+i*50;               // y coordinate
-      var radius         = 20;                    // Arc radius
-      var startAngle     = 0;                     // Starting point on circle
-      var endAngle       = Math.PI+(Math.PI*j)/2; // End point on circle
-      var anticlockwise  = i%2==0 ? false : true; // clockwise or anticlockwise
- 
-      ctx.arc(x,y,radius,startAngle,endAngle, anticlockwise);
- 
-      if ((i+j)%2==0){
+      var x = 25 + j * 50; // x coordinate
+      var y = 25 + i * 50; // y coordinate
+      var radius = 20; // Arc radius
+      var startAngle = 0; // Starting point on circle
+      var endAngle = Math.PI + (Math.PI * j) / 2; // End point on circle
+      var anticlockwise = i % 2 == 0 ? false : true; // clockwise or anticlockwise
+
+      ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
+
+      if ((i + j) % 2 == 0) {
         ctx.fill();
       } else {
         ctx.stroke();
@@ -78,13 +148,13 @@ to_load.push(Circles)
 function Quadratic() {
   var ctx = addCanvas().getContext('2d');
   ctx.beginPath();
-  ctx.moveTo(75,25);
-  ctx.quadraticCurveTo(25,25,25,62.5);
-  ctx.quadraticCurveTo(25,100,50,100);
-  ctx.quadraticCurveTo(50,120,30,125);
-  ctx.quadraticCurveTo(60,120,65,100);
-  ctx.quadraticCurveTo(125,100,125,62.5);
-  ctx.quadraticCurveTo(125,25,75,25);
+  ctx.moveTo(75, 25);
+  ctx.quadraticCurveTo(25, 25, 25, 62.5);
+  ctx.quadraticCurveTo(25, 100, 50, 100);
+  ctx.quadraticCurveTo(50, 120, 30, 125);
+  ctx.quadraticCurveTo(60, 120, 65, 100);
+  ctx.quadraticCurveTo(125, 100, 125, 62.5);
+  ctx.quadraticCurveTo(125, 25, 75, 25);
   ctx.stroke();
 }
 to_load.push(Quadratic)
@@ -92,86 +162,125 @@ to_load.push(Quadratic)
 function Bazier() {
   var ctx = addCanvas().getContext('2d');
   ctx.beginPath();
-  ctx.moveTo(75,40);
-  ctx.bezierCurveTo(75,37,70,25,50,25);
-  ctx.bezierCurveTo(20,25,20,62.5,20,62.5);
-  ctx.bezierCurveTo(20,80,40,102,75,120);
-  ctx.bezierCurveTo(110,102,130,80,130,62.5);
-  ctx.bezierCurveTo(130,62.5,130,25,100,25);
-  ctx.bezierCurveTo(85,25,75,37,75,40);
+  ctx.moveTo(75, 40);
+  ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
+  ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
+  ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
+  ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
+  ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
+  ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
   ctx.fill();
 }
 to_load.push(Bazier)
 
 function Pac() {
+
+  function roundedRect(ctx, x, y, width, height, radius) {
+    ctx.beginPath();
+    ctx.moveTo(x, y + radius);
+    ctx.lineTo(x, y + height - radius);
+    ctx.quadraticCurveTo(x, y + height, x + radius, y + height);
+    ctx.lineTo(x + width - radius, y + height);
+    ctx.quadraticCurveTo(x + width, y + height, x + width, y + height - radius);
+    ctx.lineTo(x + width, y + radius);
+    ctx.quadraticCurveTo(x + width, y, x + width - radius, y);
+    ctx.lineTo(x + radius, y);
+    ctx.quadraticCurveTo(x, y, x, y + radius);
+    ctx.stroke();
+  }
+
   var ctx = addCanvas().getContext('2d');
-  roundedRect(ctx,12,12,150,150,15);
-  roundedRect(ctx,19,19,150,150,9);
-  roundedRect(ctx,53,53,49,33,10);
-  roundedRect(ctx,53,119,49,16,6);
-  roundedRect(ctx,135,53,49,33,10);
-  roundedRect(ctx,135,119,25,49,10);
- 
+  roundedRect(ctx, 12, 12, 150, 150, 15);
+  roundedRect(ctx, 19, 19, 150, 150, 9);
+  roundedRect(ctx, 53, 53, 49, 33, 10);
+  roundedRect(ctx, 53, 119, 49, 16, 6);
+  roundedRect(ctx, 135, 53, 49, 33, 10);
+  roundedRect(ctx, 135, 119, 25, 49, 10);
+
   ctx.beginPath();
-  ctx.arc(37,37,13,Math.PI/7,-Math.PI/7,false);
-  ctx.lineTo(31,37);
+  ctx.arc(37, 37, 13, Math.PI / 7, -Math.PI / 7, false);
+  ctx.lineTo(31, 37);
   ctx.fill();
-  for(var i=0;i<8;i++){
-    ctx.fillRect(51+i*16,35,4,4);
+  for (var i = 0; i < 8; i++) {
+    ctx.fillRect(51 + i * 16, 35, 4, 4);
   }
-  for(i=0;i<6;i++){
-    ctx.fillRect(115,51+i*16,4,4);
+  for (i = 0; i < 6; i++) {
+    ctx.fillRect(115, 51 + i * 16, 4, 4);
   }
-  for(i=0;i<8;i++){
-    ctx.fillRect(51+i*16,99,4,4);
+  for (i = 0; i < 8; i++) {
+    ctx.fillRect(51 + i * 16, 99, 4, 4);
   }
   ctx.beginPath();
-  ctx.moveTo(83,116);
-  ctx.lineTo(83,102);
-  ctx.bezierCurveTo(83,94,89,88,97,88);
-  ctx.bezierCurveTo(105,88,111,94,111,102);
-  ctx.lineTo(111,116);
-  ctx.lineTo(106.333,111.333);
-  ctx.lineTo(101.666,116);
-  ctx.lineTo(97,111.333);
-  ctx.lineTo(92.333,116);
-  ctx.lineTo(87.666,111.333);
-  ctx.lineTo(83,116);
+  ctx.moveTo(83, 116);
+  ctx.lineTo(83, 102);
+  ctx.bezierCurveTo(83, 94, 89, 88, 97, 88);
+  ctx.bezierCurveTo(105, 88, 111, 94, 111, 102);
+  ctx.lineTo(111, 116);
+  ctx.lineTo(106.333, 111.333);
+  ctx.lineTo(101.666, 116);
+  ctx.lineTo(97, 111.333);
+  ctx.lineTo(92.333, 116);
+  ctx.lineTo(87.666, 111.333);
+  ctx.lineTo(83, 116);
   ctx.fill();
   ctx.fillStyle = "white";
   ctx.beginPath();
-  ctx.moveTo(91,96);
-  ctx.bezierCurveTo(88,96,87,99,87,101);
-  ctx.bezierCurveTo(87,103,88,106,91,106);
-  ctx.bezierCurveTo(94,106,95,103,95,101);
-  ctx.bezierCurveTo(95,99,94,96,91,96);
-  ctx.moveTo(103,96);
-  ctx.bezierCurveTo(100,96,99,99,99,101);
-  ctx.bezierCurveTo(99,103,100,106,103,106);
-  ctx.bezierCurveTo(106,106,107,103,107,101);
-  ctx.bezierCurveTo(107,99,106,96,103,96);
+  ctx.moveTo(91, 96);
+  ctx.bezierCurveTo(88, 96, 87, 99, 87, 101);
+  ctx.bezierCurveTo(87, 103, 88, 106, 91, 106);
+  ctx.bezierCurveTo(94, 106, 95, 103, 95, 101);
+  ctx.bezierCurveTo(95, 99, 94, 96, 91, 96);
+  ctx.moveTo(103, 96);
+  ctx.bezierCurveTo(100, 96, 99, 99, 99, 101);
+  ctx.bezierCurveTo(99, 103, 100, 106, 103, 106);
+  ctx.bezierCurveTo(106, 106, 107, 103, 107, 101);
+  ctx.bezierCurveTo(107, 99, 106, 96, 103, 96);
   ctx.fill();
   ctx.fillStyle = "black";
   ctx.beginPath();
-  ctx.arc(101,102,2,0,Math.PI*2,true);
+  ctx.arc(101, 102, 2, 0, Math.PI * 2, true);
   ctx.fill();
   ctx.beginPath();
-  ctx.arc(89,102,2,0,Math.PI*2,true);
+  ctx.arc(89, 102, 2, 0, Math.PI * 2, true);
   ctx.fill();
 }
-function roundedRect(ctx,x,y,width,height,radius){
-  ctx.beginPath();
-  ctx.moveTo(x,y+radius);
-  ctx.lineTo(x,y+height-radius);
-  ctx.quadraticCurveTo(x,y+height,x+radius,y+height);
-  ctx.lineTo(x+width-radius,y+height);
-  ctx.quadraticCurveTo(x+width,y+height,x+width,y+height-radius);
-  ctx.lineTo(x+width,y+radius);
-  ctx.quadraticCurveTo(x+width,y,x+width-radius,y);
-  ctx.lineTo(x+radius,y);
-  ctx.quadraticCurveTo(x,y,x,y+radius);
-  ctx.stroke();
-}
+
+
 
 to_load.push(Pac)
 
+function ImageDraw() {
+  var ctx = addCanvas('imgID').getContext('2d');
+  var img = new Image();
+
+  img.onload = function() {
+    for (var i = 0; i < 2; i++) {
+      for (var j = 0; j < 5; j++) {
+        if ((i + j) % 2 == 0) {
+          ctx.drawImage(img, j * 62, i * 71, 62, 71);
+        } else {
+          ctx.drawImage(img, 10, 10, 30, 30, j * 62, i * 71, 30, 30);
+        }
+      }
+    }
+  }
+  img.src = 'https://developer.cdn.mozilla.net/media/img/mdn-logo-sm.png';
+}
+to_load.push(ImageDraw);
+
+function ImageDrawRhino() {
+  var ctx = addCanvas('imgID').getContext('2d');
+  var img_1 = new Image();
+  var img_2 = new Image();
+
+  img_1.onload = function() {
+    ctx.drawImage(img_1, 33, 71, 104, 124, 60, 20, 87, 104);
+  }
+  img_2.onload = function() {
+    ctx.drawImage(img_2, 40, 0);
+  }
+  img_1.src = 'https://developer.mozilla.org/files/4533/rhino.jpg';
+  img_2.src = 'https://developer.mozilla.org/files/242/Canvas_picture_frame.png';
+
+}
+to_load.push(ImageDrawRhino);
